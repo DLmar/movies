91c7f4f9b12f477103744fc39d4d57a47289d71a
DLmar <dlmar2code@gmail.com>
2022-08-23 18:38:40 +0300

    ;

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-const App = () => {
+import {FC} from "react";
+import {Route, Routes} from "react-router-dom";
+import {Layout, MovieDetailsPage, MovieListByFilmNamePage, MovieListByGenrePage, MoviesList} from "./components";
+
+
+const App:FC  = () => {
   return (
       <div>
-        App
+        <Routes>
+            <Route path={'/'} element={<Layout/>}>
+
+                <Route path={'/'} element={<MoviesList/>}/>
+                <Route path={'/movie/:id'} element={<MovieDetailsPage/>}/>
+                <Route path={'/genre/:id'} element={<MovieListByGenrePage/>}/>
+                <Route path={'/ByFilmName'} element={<MovieListByFilmNamePage/>}/>
+
+            </Route>
+        </Routes>
       </div>
   );
 };
